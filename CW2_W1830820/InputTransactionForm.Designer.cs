80cb07d5466b85e1cc5f67d8a709d6629cfd1100
mkappworks <76746881+mkappworks@users.noreply.github.com>
2021-04-15 11:47:33 +0530

    update transaction ui

--- CW2_W1830820/InputTransactionForm.Designer.cs
+++ CW2_W1830820/InputTransactionForm.Designer.cs
@@ -29,19 +29,166 @@ namespace CW2_W1830820
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnIncome = new System.Windows.Forms.RadioButton();
+            this.radioBtnExpense = new System.Windows.Forms.RadioButton();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxContact = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtnIncome);
+            this.groupBox1.Controls.Add(this.radioBtnExpense);
+            this.groupBox1.Location = new System.Drawing.Point(115, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 46);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioBtnIncome
+            // 
+            this.radioBtnIncome.AutoSize = true;
+            this.radioBtnIncome.Location = new System.Drawing.Point(113, 14);
+            this.radioBtnIncome.Name = "radioBtnIncome";
+            this.radioBtnIncome.Size = new System.Drawing.Size(60, 17);
+            this.radioBtnIncome.TabIndex = 1;
+            this.radioBtnIncome.Text = "Income";
+            this.radioBtnIncome.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnExpense
+            // 
+            this.radioBtnExpense.AutoSize = true;
+            this.radioBtnExpense.Checked = true;
+            this.radioBtnExpense.Location = new System.Drawing.Point(22, 14);
+            this.radioBtnExpense.Name = "radioBtnExpense";
+            this.radioBtnExpense.Size = new System.Drawing.Size(66, 17);
+            this.radioBtnExpense.TabIndex = 0;
+            this.radioBtnExpense.TabStop = true;
+            this.radioBtnExpense.Text = "Expense";
+            this.radioBtnExpense.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSave.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSave.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.Location = new System.Drawing.Point(95, 215);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(169, 49);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Date";
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(115, 160);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(191, 20);
+            this.textBoxAmount.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Amount";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(115, 35);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Contact";
+            // 
+            // comboBoxContact
+            // 
+            this.comboBoxContact.FormattingEnabled = true;
+            this.comboBoxContact.Location = new System.Drawing.Point(115, 119);
+            this.comboBoxContact.Name = "comboBoxContact";
+            this.comboBoxContact.Size = new System.Drawing.Size(191, 21);
+            this.comboBoxContact.TabIndex = 15;
             // 
             // InputTransactionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(360, 312);
+            this.Controls.Add(this.comboBoxContact);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "InputTransactionForm";
             this.Text = "Input Transaction Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnIncome;
+        private System.Windows.Forms.RadioButton radioBtnExpense;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxContact;
     }
 }
