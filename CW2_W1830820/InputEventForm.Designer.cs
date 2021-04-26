1dded53e2e738f9eb43f1e34f69fc9f7baf886b9
Malith <info.mkappworks>
2021-04-26 20:23:23 +0530

    update SaveEvent in InputEventForm

--- CW2_W1830820/InputEventForm.Designer.cs
+++ CW2_W1830820/InputEventForm.Designer.cs
@@ -29,45 +29,21 @@ namespace CW2_W1830820
         /// </summary>
         private void InitializeComponent()
         {
-            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
-            this.textBoxAmount = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radioButtonTask = new System.Windows.Forms.RadioButton();
-            this.radioButtonAppointment = new System.Windows.Forms.RadioButton();
-            this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
-            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAdditionalRecurring = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.comboBoxOccurrenceType = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioBtnTask = new System.Windows.Forms.RadioButton();
+            this.radioBtnAppointment = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // dateTimePickerStartDate
-            // 
-            this.dateTimePickerStartDate.Location = new System.Drawing.Point(200, 67);
-            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
-            this.dateTimePickerStartDate.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePickerStartDate.TabIndex = 22;
-            // 
-            // textBoxAmount
-            // 
-            this.textBoxAmount.Location = new System.Drawing.Point(200, 138);
-            this.textBoxAmount.Name = "textBoxAmount";
-            this.textBoxAmount.Size = new System.Drawing.Size(191, 20);
-            this.textBoxAmount.TabIndex = 21;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(38, 137);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 20);
-            this.label3.TabIndex = 20;
-            this.label3.Text = "Description";
             // 
             // btnSave
             // 
@@ -85,110 +61,135 @@ namespace CW2_W1830820
             this.btnSave.TabIndex = 18;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.SaveEvent);
             // 
-            // label1
+            // textBoxAdditionalRecurring
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 20);
-            this.label1.TabIndex = 16;
-            this.label1.Text = "Start Date";
+            this.textBoxAdditionalRecurring.Location = new System.Drawing.Point(218, 115);
+            this.textBoxAdditionalRecurring.Name = "textBoxAdditionalRecurring";
+            this.textBoxAdditionalRecurring.Size = new System.Drawing.Size(191, 20);
+            this.textBoxAdditionalRecurring.TabIndex = 52;
             // 
-            // groupBox2
+            // label2
             // 
-            this.groupBox2.Controls.Add(this.radioButtonTask);
-            this.groupBox2.Controls.Add(this.radioButtonAppointment);
-            this.groupBox2.Location = new System.Drawing.Point(200, 175);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(191, 46);
-            this.groupBox2.TabIndex = 21;
-            this.groupBox2.TabStop = false;
-            // 
-            // radioButtonTask
-            // 
-            this.radioButtonTask.AutoSize = true;
-            this.radioButtonTask.Location = new System.Drawing.Point(113, 14);
-            this.radioButtonTask.Name = "radioButtonTask";
-            this.radioButtonTask.Size = new System.Drawing.Size(49, 17);
-            this.radioButtonTask.TabIndex = 1;
-            this.radioButtonTask.Text = "Task";
-            this.radioButtonTask.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonAppointment
-            // 
-            this.radioButtonAppointment.AutoSize = true;
-            this.radioButtonAppointment.Checked = true;
-            this.radioButtonAppointment.Location = new System.Drawing.Point(22, 14);
-            this.radioButtonAppointment.Name = "radioButtonAppointment";
-            this.radioButtonAppointment.Size = new System.Drawing.Size(84, 17);
-            this.radioButtonAppointment.TabIndex = 0;
-            this.radioButtonAppointment.TabStop = true;
-            this.radioButtonAppointment.Text = "Appointment";
-            this.radioButtonAppointment.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(38, 185);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(98, 20);
-            this.label5.TabIndex = 20;
-            this.label5.Text = "Event Type";
-            // 
-            // dateTimePickerEndDate
-            // 
-            this.dateTimePickerEndDate.Location = new System.Drawing.Point(200, 103);
-            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
-            this.dateTimePickerEndDate.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePickerEndDate.TabIndex = 24;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(38, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 20);
-            this.label4.TabIndex = 23;
-            this.label4.Text = "End Date";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 20);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Additional Recurring";
             // 
             // comboBoxOccurrenceType
             // 
             this.comboBoxOccurrenceType.FormattingEnabled = true;
-            this.comboBoxOccurrenceType.Location = new System.Drawing.Point(200, 31);
+            this.comboBoxOccurrenceType.Location = new System.Drawing.Point(218, 41);
             this.comboBoxOccurrenceType.Name = "comboBoxOccurrenceType";
             this.comboBoxOccurrenceType.Size = new System.Drawing.Size(191, 21);
-            this.comboBoxOccurrenceType.TabIndex = 26;
+            this.comboBoxOccurrenceType.TabIndex = 50;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(38, 31);
+            this.label6.Location = new System.Drawing.Point(32, 39);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(144, 20);
-            this.label6.TabIndex = 25;
+            this.label6.TabIndex = 49;
             this.label6.Text = "Occurrence Type";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioBtnTask);
+            this.groupBox2.Controls.Add(this.radioBtnAppointment);
+            this.groupBox2.Location = new System.Drawing.Point(218, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(191, 46);
+            this.groupBox2.TabIndex = 46;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioBtnTask
+            // 
+            this.radioBtnTask.AutoSize = true;
+            this.radioBtnTask.Location = new System.Drawing.Point(113, 14);
+            this.radioBtnTask.Name = "radioBtnTask";
+            this.radioBtnTask.Size = new System.Drawing.Size(49, 17);
+            this.radioBtnTask.TabIndex = 1;
+            this.radioBtnTask.Text = "Task";
+            this.radioBtnTask.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnAppointment
+            // 
+            this.radioBtnAppointment.AutoSize = true;
+            this.radioBtnAppointment.Checked = true;
+            this.radioBtnAppointment.Location = new System.Drawing.Point(22, 14);
+            this.radioBtnAppointment.Name = "radioBtnAppointment";
+            this.radioBtnAppointment.Size = new System.Drawing.Size(84, 17);
+            this.radioBtnAppointment.TabIndex = 0;
+            this.radioBtnAppointment.TabStop = true;
+            this.radioBtnAppointment.Text = "Appointment";
+            this.radioBtnAppointment.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 20);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Event Type";
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(218, 77);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerStartDate.TabIndex = 48;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(218, 148);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(191, 20);
+            this.textBoxDescription.TabIndex = 47;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Start Date";
             // 
             // InputEventForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(440, 349);
+            this.Controls.Add(this.textBoxAdditionalRecurring);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBoxOccurrenceType);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.dateTimePickerEndDate);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.dateTimePickerStartDate);
-            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.textBoxDescription);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
             this.Name = "InputEventForm";
             this.Text = "InputEventForm";
             this.groupBox2.ResumeLayout(false);
@@ -199,18 +200,18 @@ namespace CW2_W1830820
         }
 
         #endregion
-        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
-        private System.Windows.Forms.TextBox textBoxAmount;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton radioButtonTask;
-        private System.Windows.Forms.RadioButton radioButtonAppointment;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePickerEndDate;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAdditionalRecurring;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBoxOccurrenceType;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioBtnTask;
+        private System.Windows.Forms.RadioButton radioBtnAppointment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- CW2_W1830820/InputEventForm.cs
+++ CW2_W1830820/InputEventForm.cs
@@ -7,14 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CW2_W1830820
 {
     public partial class InputEventForm : Form
     {
+        private DBManager dbManager = new DBManager();
+        public EventDetails EventDetailsData { get; set; }
         public InputEventForm()
         {
             InitializeComponent();
+
+            this.radioBtnAppointment.Checked = true;
+        }
+
+        private void SaveEvent(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to save the new event?", "PFMS | Save Event", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+                this.EventDetailsData.OccurrenceType = (string)this.comboBoxOccurrenceType.SelectedItem;
+                this.EventDetailsData.StartDate = this.dateTimePickerStartDate.Value;
+                this.EventDetailsData.NumberOfAdditionalTimesRecurring = int.Parse(this.textBoxAdditionalRecurring.Text);
+                this.EventDetailsData.Description = this.textBoxDescription.Text;
+
+                if (this.radioBtnAppointment.Checked)
+                {
+                    this.EventDetailsData.EventType = "Appointment";
+                }
+                else if (this.radioBtnTask.Checked)
+                {
+                    this.EventDetailsData.EventType = "Task";
+                }
+
+                if (File.Exists(@"eventinputdata.xml"))
+                {
+                    this.dbManager.ReadXml(@"eventinputdata.xml");
+                }
+
+                DBManager.EventHeaderRow row = this.dbManager.EventHeader.NewEventHeaderRow();
+
+
+                row.OccurrenceType = this.EventDetailsData.OccurrenceType;
+                row.StartDate = this.EventDetailsData.StartDate;
+                row.NumberOfAdditionalTimesRecurring_ = this.EventDetailsData.NumberOfAdditionalTimesRecurring;
+                row.Description = this.EventDetailsData.Description;
+                row.EventType = this.EventDetailsData.EventType;
+
+                this.dbManager.EventHeader.AddEventHeaderRow(row);
+                this.dbManager.AcceptChanges();
+
+                this.dbManager.WriteXml(@"eventinputdata.xml");
+
+                EventModel eventModel = new EventModel();
+
+                eventModel.SaveEvent(EventDetailsData);
+
+                this.dbManager.Reset();
+                File.Delete(@"eventinputdata.xml");
+
+                MessageBox.Show("Successfully Saved");
+
+              
+
+                this.radioBtnAppointment.Checked = true;
+                this.textBoxAdditionalRecurring.Clear();
+                this.textBoxDescription.Clear();
+
+            }
         }
     }
 }
