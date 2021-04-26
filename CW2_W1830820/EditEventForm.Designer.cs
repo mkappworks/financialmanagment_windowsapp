6535a0e045df06ef72b8cd87953137551688a45d
Malith <info.mkappworks>
2021-04-26 19:27:40 +0530

    changes to EditEventForm UI

--- CW2_W1830820/EditEventForm.Designer.cs
+++ CW2_W1830820/EditEventForm.Designer.cs
@@ -40,20 +40,15 @@ namespace CW2_W1830820
             this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
             this.textBoxAmount = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioBtnRecurring = new System.Windows.Forms.RadioButton();
-            this.radioBtnOneOff = new System.Windows.Forms.RadioButton();
             this.btnEdit = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxOccurrenceType
             // 
             this.comboBoxOccurrenceType.FormattingEnabled = true;
-            this.comboBoxOccurrenceType.Location = new System.Drawing.Point(187, 81);
+            this.comboBoxOccurrenceType.Location = new System.Drawing.Point(184, 33);
             this.comboBoxOccurrenceType.Name = "comboBoxOccurrenceType";
             this.comboBoxOccurrenceType.Size = new System.Drawing.Size(191, 21);
             this.comboBoxOccurrenceType.TabIndex = 39;
@@ -62,7 +57,7 @@ namespace CW2_W1830820
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(25, 81);
+            this.label6.Location = new System.Drawing.Point(22, 33);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(144, 20);
             this.label6.TabIndex = 38;
@@ -70,7 +65,7 @@ namespace CW2_W1830820
             // 
             // dateTimePickerEndDate
             // 
-            this.dateTimePickerEndDate.Location = new System.Drawing.Point(187, 153);
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(184, 105);
             this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
             this.dateTimePickerEndDate.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerEndDate.TabIndex = 37;
@@ -79,7 +74,7 @@ namespace CW2_W1830820
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(25, 152);
+            this.label4.Location = new System.Drawing.Point(22, 104);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 20);
             this.label4.TabIndex = 36;
@@ -89,7 +84,7 @@ namespace CW2_W1830820
             // 
             this.groupBox2.Controls.Add(this.radioButtonTask);
             this.groupBox2.Controls.Add(this.radioButtonAppointment);
-            this.groupBox2.Location = new System.Drawing.Point(187, 225);
+            this.groupBox2.Location = new System.Drawing.Point(184, 177);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(191, 46);
             this.groupBox2.TabIndex = 33;
@@ -121,7 +116,7 @@ namespace CW2_W1830820
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(25, 235);
+            this.label5.Location = new System.Drawing.Point(22, 187);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 20);
             this.label5.TabIndex = 31;
@@ -129,14 +124,14 @@ namespace CW2_W1830820
             // 
             // dateTimePickerStartDate
             // 
-            this.dateTimePickerStartDate.Location = new System.Drawing.Point(187, 117);
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(184, 69);
             this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
             this.dateTimePickerStartDate.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerStartDate.TabIndex = 35;
             // 
             // textBoxAmount
             // 
-            this.textBoxAmount.Location = new System.Drawing.Point(187, 188);
+            this.textBoxAmount.Location = new System.Drawing.Point(184, 140);
             this.textBoxAmount.Name = "textBoxAmount";
             this.textBoxAmount.Size = new System.Drawing.Size(191, 20);
             this.textBoxAmount.TabIndex = 34;
@@ -145,43 +140,11 @@ namespace CW2_W1830820
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(25, 187);
+            this.label3.Location = new System.Drawing.Point(22, 139);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 20);
             this.label3.TabIndex = 32;
             this.label3.Text = "Description";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.radioBtnRecurring);
-            this.groupBox1.Controls.Add(this.radioBtnOneOff);
-            this.groupBox1.Location = new System.Drawing.Point(187, 20);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(191, 46);
-            this.groupBox1.TabIndex = 30;
-            this.groupBox1.TabStop = false;
-            // 
-            // radioBtnRecurring
-            // 
-            this.radioBtnRecurring.AutoSize = true;
-            this.radioBtnRecurring.Location = new System.Drawing.Point(113, 14);
-            this.radioBtnRecurring.Name = "radioBtnRecurring";
-            this.radioBtnRecurring.Size = new System.Drawing.Size(71, 17);
-            this.radioBtnRecurring.TabIndex = 1;
-            this.radioBtnRecurring.Text = "Recurring";
-            this.radioBtnRecurring.UseVisualStyleBackColor = true;
-            // 
-            // radioBtnOneOff
-            // 
-            this.radioBtnOneOff.AutoSize = true;
-            this.radioBtnOneOff.Checked = true;
-            this.radioBtnOneOff.Location = new System.Drawing.Point(22, 14);
-            this.radioBtnOneOff.Name = "radioBtnOneOff";
-            this.radioBtnOneOff.Size = new System.Drawing.Size(59, 17);
-            this.radioBtnOneOff.TabIndex = 0;
-            this.radioBtnOneOff.TabStop = true;
-            this.radioBtnOneOff.Text = "OneOff";
-            this.radioBtnOneOff.UseVisualStyleBackColor = true;
             // 
             // btnEdit
             // 
@@ -193,7 +156,7 @@ namespace CW2_W1830820
             this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEdit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnEdit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnEdit.Location = new System.Drawing.Point(115, 315);
+            this.btnEdit.Location = new System.Drawing.Point(97, 253);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(169, 49);
             this.btnEdit.TabIndex = 29;
@@ -201,21 +164,11 @@ namespace CW2_W1830820
             this.btnEdit.UseVisualStyleBackColor = false;
             this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(25, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 20);
-            this.label2.TabIndex = 28;
-            this.label2.Text = "Occurrence";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(25, 116);
+            this.label1.Location = new System.Drawing.Point(22, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(93, 20);
             this.label1.TabIndex = 27;
@@ -235,16 +188,12 @@ namespace CW2_W1830820
             this.Controls.Add(this.dateTimePickerStartDate);
             this.Controls.Add(this.textBoxAmount);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnEdit);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "EditEventForm";
             this.Text = "EditEventForm";
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -263,11 +212,7 @@ namespace CW2_W1830820
         private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
         private System.Windows.Forms.TextBox textBoxAmount;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioBtnRecurring;
-        private System.Windows.Forms.RadioButton radioBtnOneOff;
         private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
 }
