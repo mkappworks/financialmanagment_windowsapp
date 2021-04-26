734e87b855c14fcc4dc7675644812d8e0d1bc92f
Malith <info.mkappworks>
2021-04-26 19:26:32 +0530

    update ViewEvent

--- CW2_W1830820/ViewEventForm.Designer.cs
+++ CW2_W1830820/ViewEventForm.Designer.cs
@@ -30,102 +30,126 @@ namespace CW2_W1830820
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Occurrence = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewEvent = new System.Windows.Forms.DataGridView();
+            this.transactionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eventDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eventOccurrenceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.startDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eventTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Edit = new System.Windows.Forms.DataGridViewButtonColumn();
             this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
-        
-            this.transactionBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-     
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvent)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.transactionBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventDetailsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewEvent
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Date,
-            this.Description,
-            this.Occurrence,
-            this.Type,
+            this.dataGridViewEvent.AllowUserToAddRows = false;
+            this.dataGridViewEvent.AllowUserToDeleteRows = false;
+            this.dataGridViewEvent.AutoGenerateColumns = false;
+            this.dataGridViewEvent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEvent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.eventOccurrenceDataGridViewTextBoxColumn,
+            this.startDateDataGridViewTextBoxColumn,
+            this.numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.eventTypeDataGridViewTextBoxColumn,
             this.Edit,
             this.Delete});
-            this.dataGridView1.DataSource = this.transactionBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(42, 43);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(646, 206);
-            this.dataGridView1.TabIndex = 1;
+            this.dataGridViewEvent.DataSource = this.eventDetailsBindingSource;
+            this.dataGridViewEvent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewEvent.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewEvent.Name = "dataGridViewEvent";
+            this.dataGridViewEvent.ReadOnly = true;
+            this.dataGridViewEvent.Size = new System.Drawing.Size(742, 450);
+            this.dataGridViewEvent.TabIndex = 1;
+            this.dataGridViewEvent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // Date
+            // eventDetailsBindingSource
             // 
-            this.Date.HeaderText = "Date";
-            this.Date.Name = "Date";
+            this.eventDetailsBindingSource.DataSource = typeof(CW2_W1830820.EventDetails);
             // 
-            // Description
+            // eventOccurrenceDataGridViewTextBoxColumn
             // 
-            this.Description.HeaderText = "Description";
-            this.Description.Name = "Description";
+            this.eventOccurrenceDataGridViewTextBoxColumn.DataPropertyName = "EventOccurrence";
+            this.eventOccurrenceDataGridViewTextBoxColumn.HeaderText = "EventOccurrence";
+            this.eventOccurrenceDataGridViewTextBoxColumn.Name = "eventOccurrenceDataGridViewTextBoxColumn";
+            this.eventOccurrenceDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // Occurrence
+            // startDateDataGridViewTextBoxColumn
             // 
-            this.Occurrence.HeaderText = "Occurrence";
-            this.Occurrence.Name = "Occurrence";
+            this.startDateDataGridViewTextBoxColumn.DataPropertyName = "StartDate";
+            this.startDateDataGridViewTextBoxColumn.HeaderText = "StartDate";
+            this.startDateDataGridViewTextBoxColumn.Name = "startDateDataGridViewTextBoxColumn";
+            this.startDateDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // Type
+            // numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn
             // 
-            this.Type.HeaderText = "Type";
-            this.Type.Name = "Type";
+            this.numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn.DataPropertyName = "NumberOfAdditionalTimesRecurring";
+            this.numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn.HeaderText = "NumberOfAdditionalTimesRecurring";
+            this.numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn.Name = "numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn";
+            this.numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // eventTypeDataGridViewTextBoxColumn
+            // 
+            this.eventTypeDataGridViewTextBoxColumn.DataPropertyName = "EventType";
+            this.eventTypeDataGridViewTextBoxColumn.HeaderText = "EventType";
+            this.eventTypeDataGridViewTextBoxColumn.Name = "eventTypeDataGridViewTextBoxColumn";
+            this.eventTypeDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // Edit
             // 
             this.Edit.HeaderText = "Edit";
             this.Edit.Name = "Edit";
+            this.Edit.ReadOnly = true;
+            this.Edit.Text = "Edit";
+            this.Edit.UseColumnTextForButtonValue = true;
             // 
             // Delete
             // 
             this.Delete.HeaderText = "Delete";
             this.Delete.Name = "Delete";
-            // 
-            // transaction
-            // 
-           
-            // 
-            // transactionBindingSource
-            // 
-         
-            this.transactionBindingSource.Position = 0;
+            this.Delete.ReadOnly = true;
+            this.Delete.Text = "Delete";
+            this.Delete.UseColumnTextForButtonValue = true;
             // 
             // ViewEventForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(742, 450);
+            this.Controls.Add(this.dataGridViewEvent);
             this.Name = "ViewEventForm";
             this.Text = "ViewEventForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-         
+            this.Load += new System.EventHandler(this.ViewEventForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEvent)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.transactionBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eventDetailsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Occurrence;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+        private System.Windows.Forms.DataGridView dataGridViewEvent;
+        private System.Windows.Forms.BindingSource transactionBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eventOccurrenceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numberOfAdditionalTimesRecurringDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eventTypeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewButtonColumn Edit;
         private System.Windows.Forms.DataGridViewButtonColumn Delete;
-        private System.Windows.Forms.BindingSource transactionBindingSource;
- 
+        private System.Windows.Forms.BindingSource eventDetailsBindingSource;
     }
 }
--- CW2_W1830820/ViewEventForm.cs
+++ CW2_W1830820/ViewEventForm.cs
@@ -12,9 +12,98 @@ namespace CW2_W1830820
 {
     public partial class ViewEventForm : Form
     {
+        public EventDetails EventDetailsData { get; set; }
+
         public ViewEventForm()
         {
             InitializeComponent();
         }
+
+
+
+        private void ViewEventForm_Load(object sender, EventArgs e)
+        {
+            GetEventData();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (dataGridViewEvent.Columns[e.ColumnIndex].Name == "Edit")
+            {
+
+                int selectId = (int)dataGridViewEvent.Rows[e.RowIndex].Cells[0].Value;
+                string selectEventOccurrence = (string)dataGridViewEvent.Rows[e.RowIndex].Cells[1].Value;
+                DateTime selectStartDate = (DateTime)dataGridViewEvent.Rows[e.RowIndex].Cells[2].Value;
+                int selectNumberOfAdditionalTimesRecurring = (int)dataGridViewEvent.Rows[e.RowIndex].Cells[3].Value;
+                string selectDescription = (string)dataGridViewEvent.Rows[e.RowIndex].Cells[4].Value;
+                string selectEventType = (string)dataGridViewEvent.Rows[e.RowIndex].Cells[5].Value;
+
+
+                this.EventDetailsData = new EventDetails();
+                this.EventDetailsData.Id = selectId;
+                this.EventDetailsData.EventOccurrence = selectEventOccurrence;
+                this.EventDetailsData.StartDate = selectStartDate;
+                this.EventDetailsData.NumberOfAdditionalTimesRecurring = selectNumberOfAdditionalTimesRecurring;
+                this.EventDetailsData.Description = selectDescription;
+                this.EventDetailsData.EventType = selectEventType;
+
+                EditEventForm editEventForm = new EditEventForm(EventDetailsData);
+
+                editEventForm.Activate();
+                editEventForm.ShowDialog();
+
+                GetEventData();
+
+
+            }
+
+            if (dataGridViewEvent.Columns[e.ColumnIndex].Name == "Delete")
+            {
+
+
+                if (MessageBox.Show("Do you want to delete the selected event?", "PFMS | Delete Event", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+
+                    int selectId = (int)dataGridViewEvent.Rows[e.RowIndex].Cells[0].Value;
+
+                    EventModel eventModel = new EventModel();
+                    eventModel.DeleteEvent(selectId);
+                    
+                    MessageBox.Show("Successfully Deleted");
+
+                    eventDetailsBindingSource.RemoveCurrent();
+                }
+
+            }
+        }
+
+        private void GetEventData()
+        {
+            this.dataGridViewEvent.Rows.Clear();
+
+            EventModel eventModel = new EventModel();
+            var eventTable = eventModel.GetEvent();
+
+            foreach (var event in eventTable)
+            {
+
+            this.eventDetailsBindingSource.Add(new EventDetails()
+            {
+                Id = event.Id,
+                EventOccurrence= event.EventOccurrence ,
+                StartDate=  event.StartDate ,
+                NumberOfAdditionalTimesRecurring= event.NumberOfAdditionalTimesRecurring ,
+                Description =  event.Description ,
+                EventType= event.EventType ,
+             });
+            }
+
+
+
+
+}
+
+
     }
 }
